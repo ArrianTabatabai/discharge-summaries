93ea0f49cd8eda1f514242f61b857bb47b2fa909
Rajib Ahmed <43868221+spades1404@users.noreply.github.com>
2024-10-08 01:27:22 +0100

    requirements

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 
 
-raw_url = 'https://a293-86-10-113-94.ngrok-free.app'
+raw_url = 'https://dory-pretty-snake.ngrok-free.app/'
 req_url = raw_url + "/summarize"
 path = r"C:\Users\rajib\Documents\GitHub\discharge-summaries\Example Training\2 (Single File Format)\input"
 
